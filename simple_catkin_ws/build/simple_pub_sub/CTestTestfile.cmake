3d3eeb38f8628665f0aca45ec823b12f1aa4a2b4
hashrobotics-hr <73425974+hashrobotics-hr@users.noreply.github.com>
2021-06-12 19:39:15 -0800

    del

--- simple_catkin_ws/build/simple_pub_sub/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/simple_catkin_ws/src/simple_pub_sub
-# Build directory: /home/ubuntu/simple_catkin_ws/build/simple_pub_sub
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
