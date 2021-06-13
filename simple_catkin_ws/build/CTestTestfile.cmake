4c8e39cd8914ba26197b21c4c688456f5ce4bf4b
hashrobotics-hr <73425974+hashrobotics-hr@users.noreply.github.com>
2021-06-12 22:06:05 -0800

    ros node and topic

---
+++ simple_catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/simple_catkin_ws/src
+# Build directory: /home/ubuntu/simple_catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("simple_pub_sub")
